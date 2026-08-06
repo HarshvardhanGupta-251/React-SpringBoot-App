57e9f7fedd26a9d0726c242614491cc6b214395f
HarshvardhanGupta-251 <harshvardhangupta751@gmail.com>
2026-08-07 02:41:18 +0530

    Data of H2

--- src/main/java/com/H2_Connection/React_Spring/Boot/model/Product.java
+++ src/main/java/com/H2_Connection/React_Spring/Boot/model/Product.java
@@ -1,4 +1,17 @@
 package com.H2_Connection.React_Spring.Boot.model;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class Product {
+
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    private Date releaseDate;
+    private Boolean available;
+    private int quantity;
 }
