1a7b54c35df3bd07fcebfd410b9f850702a09081
HarshvardhanGupta-251 <harshvardhangupta751@gmail.com>
2026-08-07 02:44:54 +0530

    Updated Annotation

--- src/main/java/com/H2_Connection/React_Spring/Boot/model/Product.java
+++ src/main/java/com/H2_Connection/React_Spring/Boot/model/Product.java
@@ -1,10 +1,21 @@
 package com.H2_Connection.React_Spring.Boot.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
+    @Id
     private int id;
     private String name;
     private String description;
